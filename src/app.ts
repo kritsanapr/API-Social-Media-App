6748694e14875c01975cd8e3179a367941b9f00a
kritsanapr <kritsana.pr.41@gmail.com>
2023-02-25 13:01:35 +0700

    Setup test file

---
+++ src/app.ts
@@ -0,0 +1,67 @@
+import express, { Request, Response, NextFunction } from "express";
+import { urlencoded, json } from "body-parser";
+
+import {
+  newPostRouter,
+  deletePostRouter,
+  updatePostRouter,
+  showPostRouter,
+  newCommentRouter,
+  deleteCommentRouter,
+  deleteImageRouter,
+  addImagesRouter,
+} from "./routers";
+import cors from "cors";
+import cookieSession from "cookie-session";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import {
+  requireAuth,
+  currentUser,
+  errorHandler,
+  NotFoundError,
+} from "../common/index";
+
+const app = express();
+
+declare global {
+  interface CustomError extends Error {
+    status?: number;
+  }
+}
+
+app.use(errorHandler);
+app.set("trust proxy", true);
+app.use(
+  cookieSession({
+    signed: false,
+    secure: false,
+  })
+);
+app.use(
+  cors({
+    origin: "*",
+    optionsSuccessStatus: 200,
+  })
+);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(currentUser);
+app.use(requireAuth, newPostRouter);
+app.use(requireAuth, deletePostRouter);
+app.use(requireAuth, updatePostRouter);
+app.use(requireAuth, deleteImageRouter);
+app.use(requireAuth, addImagesRouter);
+
+app.use(showPostRouter);
+
+app.use(requireAuth, newCommentRouter);
+app.use(deleteCommentRouter);
+
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  next(new NotFoundError());
+});
+
+export { app };
--- src/main.ts
+++ src/main.ts
@@ -1,68 +1,8 @@
-import express, { Request, Response, NextFunction } from "express";
-import { urlencoded, json } from "body-parser";
-import mongoose, { Error } from "mongoose";
-import {
-  newPostRouter,
-  deletePostRouter,
-  updatePostRouter,
-  showPostRouter,
-  newCommentRouter,
-  deleteCommentRouter,
-  deleteImageRouter,
-  addImagesRouter,
-} from "./routers";
-import cors from "cors";
-import cookieSession from "cookie-session";
-import * as dotenv from "dotenv";
-dotenv.config();
+import mongoose from "mongoose";
+import { app } from "./app";
 
-import {
-  requireAuth,
-  currentUser,
-  errorHandler,
-  NotFoundError,
-} from "../common/index";
-
-const app = express();
 const port = process.env.PORT || 8080;
-declare global {
-  interface CustomError extends Error {
-    status?: number;
-  }
-}
 
-app.use(errorHandler);
-app.set("trust proxy", true);
-app.use(
-  cookieSession({
-    signed: false,
-    secure: false,
-  })
-);
-app.use(
-  cors({
-    origin: "*",
-    optionsSuccessStatus: 200,
-  })
-);
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-app.use(currentUser);
-app.use(requireAuth, newPostRouter);
-app.use(requireAuth, deletePostRouter);
-app.use(requireAuth, updatePostRouter);
-app.use(requireAuth, deleteImageRouter);
-app.use(requireAuth, addImagesRouter);
-
-app.use(showPostRouter);
-
-app.use(requireAuth, newCommentRouter);
-app.use(deleteCommentRouter);
-
-app.all("*", (req: Request, res: Response, next: NextFunction) => {
-  next(new NotFoundError());
-});
 const start = () => {
   if (!process.env.MONGO_URI) throw new Error("MONGO_URI is required");
 
